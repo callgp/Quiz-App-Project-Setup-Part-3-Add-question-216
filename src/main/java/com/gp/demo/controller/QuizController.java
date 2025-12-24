71c19b3b7564fb93ae83d4a401cb79e0273b2c60
gp <gp@mac.mynetworksettings.com>
2025-12-24 14:41:55 -0500

    first commitssssss

--- src/main/java/com/gp/demo/controller/QuizController.java
+++ src/main/java/com/gp/demo/controller/QuizController.java
@@ -4,11 +4,13 @@ import com.gp.demo.Question;
 import com.gp.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("question")
 public class QuizController {
 
     @Autowired
@@ -17,6 +19,11 @@ public class QuizController {
     public List<Question> getAllQuestions(){
          return questionService.getAllQuestions();
     }
+
+//    @GetMapping("allQuestions")
+//    public List<Question> getQuestionBYCategory(){
+//        return questionService.getAllQuestions();
+//    }
 
 
 }
